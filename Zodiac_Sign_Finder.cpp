b51086e827d39126803c9fbe7f12c706ee8e3f21
VincentNavarro <jvincentnavarro@yahoo.com>
2017-10-28 18:39:33 -0700

    Update Zodiac_Sign_Finder.cpp
    Added description

--- Zodiac_Sign_Finder.cpp
+++ Zodiac_Sign_Finder.cpp
@@ -1,3 +1,16 @@
+/*
+Author: Vincent Navarro (vincent@vincentnavarro)
+
+Date created: June 18, 2017
+
+Description:
+	- User enters month and day
+	- Makes a new integer called 'md' 
+	   - Calculated by multiplying the month by 100 and then adding the day
+	- Make comparisons and set the sign to the Zodiac Sign
+	- Print out the user's zodiac sign
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <string>
